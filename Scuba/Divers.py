61feed0e450e33ac235714be9c3ac197726de9ad
Steve Sinclair <steve_sinclair@kenan-flagler.unc.edu>
2013-04-13 15:11:12 -0400

    New idea, didn't work.
    Roll it back

--- Scuba/Divers.py
+++ Scuba/Divers.py
@@ -908,6 +908,8 @@ def restart_fish():
 totalAir = 0
 airRate = 0
 #degrees = 0
+endx = 0
+endy = 0
 
 #Notes for rotation: Radius is 150
 ########Air Consumption##########################
@@ -931,6 +933,8 @@ def consumeAir():
     global needle
     global orangedead
     #global degrees
+    global endy
+    global endx
     pi = 3.141592653589793238462643383279502884197169399375
     radius = 150 #Radius of gauge, if time try to make that actually use the gauge rect to calculate width
     print("ConsumeAir Called"+str(totalAir)+str(airRate))
@@ -938,9 +942,9 @@ def consumeAir():
     #degrees += airRate
     startx = gaugerect.centerx
     starty = gaugerect.centery
-    endx = 100*math.cos((totalAir)/180.0*pi) 
-    endy = 100*math.sin((totalAir)/180.0*pi)
-    needle = pygame.draw.line(Pressuregauge[0], (120,0,0), (startx, starty), (endx, endy), 5)
+    endx -= math.cos((airRate)/180.0*pi) 
+    endy -= math.sin((airRate)/180.0*pi)
+    needle = pygame.draw.line(Pressuregauge[0], (120,0,0), (startx, starty), (100*endx, 100*endy), 5)
     if totalAir <= 0:
         orangedead = True
 #def gaugeLinedraw():
