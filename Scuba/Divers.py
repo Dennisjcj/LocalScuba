28aefc391d60282df8694a22e467436bbc8b256a
Steve Sinclair <steve_sinclair@kenan-flagler.unc.edu>
2013-04-25 21:08:43 -0400

    Blarg

--- Scuba/Divers.py
+++ Scuba/Divers.py
@@ -944,7 +944,8 @@ degrees = 0
 def airSetup():
     
     global totalAir
-    global airRate 
+    global airRate
+    #global Pressuregauge 
     totalAir = 180
     airRate = 1
     pygame.time.set_timer(pygame.USEREVENT+1, 1000)
@@ -962,6 +963,7 @@ def consumeAir():
     global needle
     global orangedead
     global degrees
+    #global Pressuregauge
     pi = 3.141592653589793238462643383279502884197169399375
     radius = 150 #Radius of gauge, if time try to make that actually use the gauge rect to calculate width
     print("ConsumeAir Called"+str(totalAir)+str(airRate))
