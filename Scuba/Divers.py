d2d5bee331b14c683f2659149be55e3912949349
Steve Sinclair <steve_sinclair@kenan-flagler.unc.edu>
2013-04-25 21:56:25 -0400

    Github broke

--- Scuba/Divers.py
+++ Scuba/Divers.py
@@ -938,7 +938,7 @@ def restart_fish():
 totalAir = 0
 airRate = 0
 degrees = 0
-gauge_image = pygame.image.load("Pressuregauge.png") #For redrawing
+
 #Notes for rotation: Radius is 150
 ########Air Consumption##########################
 def airSetup():
@@ -946,8 +946,6 @@ def airSetup():
     global totalAir
     global airRate
     global degrees
-    global gauge_image
-    Pressuregauge[0] = gauge_image
     degrees = 0
     totalAir = 180
     airRate = 10
@@ -966,7 +964,6 @@ def consumeAir():
     global needle
     global orangedead
     global degrees
-    global gauge_image
     pi = 3.141592653589793238462643383279502884197169399375
     radius = 150 #Radius of gauge, if time try to make that actually use the gauge rect to calculate width
     print("ConsumeAir Called"+str(totalAir)+str(airRate))
@@ -976,12 +973,15 @@ def consumeAir():
     starty = gaugerect.centery
     endx = 150*math.cos((degrees*pi)/180.0) + startx
     endy = -150*math.sin((degrees*pi)/180.0)+starty
-    #Pressuregauge[0].fill
-    #Pressuregauge[0].set_alpha(255)
+    Pressuregauge[0].fill
+    Pressuregauge[0].set_alpha(255)
     #draw(Pressuregauge)
-    
+    gauge_image = pygame.image.load("Pressuregauge.png") #For redrawing
     Pressuregauge[0] = gauge_image
    # Pressuregauge[0].blit(gauge_image, (0,0))
+   # Pressuregauge[0].fill((0,0,0))
+    #draw(Pressuregauge)
+    pygame.display.flip()
     pygame.draw.line(Pressuregauge[0], (120,0,0), (startx, starty), (endx, endy), 5)
     
     if totalAir <= 0:
